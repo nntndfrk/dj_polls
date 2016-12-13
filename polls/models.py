ff26b97246d1e674ec8842eefd914ab53064f896
nntndfrk <nntndfrk@gmail.com>
2016-12-13 06:46:04 +0200

    part 2 admin site.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +12,15 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_desription = 'Published recently?'
+
 
 class Choice(models.Model):
     """Модель вибору"""
@@ -14,3 +28,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
